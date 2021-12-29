e6d5599cf659f3593e81e2f7fe7fe301399e3b94
Thomas Faurbye Nielsen <Archanium@gmail.com>
2021-12-29 12:06:31 +0100

    Updating the build script

--- .ops/build_and_push_docker_image.sh
+++ .ops/build_and_push_docker_image.sh
@@ -1,11 +1,18 @@
 #!/usr/bin/env bash
-
 if [[ "{$CIRCLE_BRANCH}x" == "masterx" ]]; then
   TAGNAME="latest"
 else
   TAGNAME="${CIRCLE_BRANCH}"
 fi
 export DOCKER_BUILDKIT=1
+UNAME="$DOCKERHUB_USERNAME"
+UPASS="$DOCKERHUB_PASS"
+
+function docker_tag_exists() {
+    TOKEN=$(curl -s -H "Content-Type: application/json" -X POST -d '{"username": "'${UNAME}'", "password": "'${UPASS}'"}' https://hub.docker.com/v2/users/login/ | jq -r .token)
+    curl --silent -f --head -lL https://hub.docker.com/v2/repositories/$1/tags/$2/ > /dev/null
+}
+
 echo "$DOCKERHUB_PASS" | docker login -u "$DOCKERHUB_USERNAME" --password-stdin;
 docker build --tag "byflou/$1:${TAGNAME}" --file Dockerfile ./$1
 
