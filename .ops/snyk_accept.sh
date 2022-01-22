6743e2130e7f6a54ce1a3c2eb01f0912826bd36c
Thomas Faurbye Nielsen <Archanium@gmail.com>
2022-01-22 12:40:50 +0100

    Install docker-scan

--- .ops/snyk_accept.sh
+++ .ops/snyk_accept.sh
@@ -4,6 +4,9 @@ if [[ "${SNYK_TOKEN}x" == "x" ]]; then
   export SNYK_ACCEPTED=0;
   exit 0;
 fi;
+mkdir -p ~/.docker/cli-plugins && \
+curl https://github.com/docker/scan-cli-plugin/releases/latest/download/docker-scan_linux_amd64 -L -s -S -o ~/.docker/cli-plugins/docker-scan && \
+chmod +x ~/.docker/cli-plugins/docker-scan
 docker scan --accept-license --token "${SNYK_TOKEN}" --login
 docker scan --version
 export SNYK_ACCEPTED=1;
