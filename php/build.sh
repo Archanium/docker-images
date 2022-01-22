7cba63f48bddd55621601652b0abc890d0246b19
Thomas Faurbye Nielsen <Archanium@gmail.com>
2022-01-22 10:48:32 +0100

    Quiet build context

--- php/build.sh
+++ php/build.sh
@@ -5,6 +5,6 @@ POSTFIX="-${LATEST_TAG}"
 if  [[ "${LATEST_TAG}x" = "latestx" ]]; then
   POSTFIX=""
 fi
-docker build --tag "byflou/${PROJECT}:php8.0${POSTFIX}" --build-arg PHP_VERSION=8.0-fpm .
-docker build --tag "byflou/${PROJECT}:php8.1${POSTFIX}" --build-arg PHP_VERSION=8.1-fpm --tag "byflou/${PROJECT}:${LATEST_TAG}" .
+docker build --quiet --tag "byflou/${PROJECT}:php8.0${POSTFIX}" --build-arg PHP_VERSION=8.0-fpm .
+docker build --quiet --tag "byflou/${PROJECT}:php8.1${POSTFIX}" --build-arg PHP_VERSION=8.1-fpm --tag "byflou/${PROJECT}:${LATEST_TAG}" .
 docker push --all-tags "byflou/${PROJECT}"
