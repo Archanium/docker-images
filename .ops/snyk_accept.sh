27a89434c764ec75a7682eb466bb09f90778239a
Thomas Faurbye Nielsen <Archanium@gmail.com>
2022-01-22 12:34:17 +0100

    Updating the snyk script

--- .ops/snyk_accept.sh
+++ .ops/snyk_accept.sh
@@ -4,7 +4,6 @@ if [[ "${SNYK_TOKEN}x" == "x" ]]; then
   export SNYK_ACCEPTED=0;
   exit 0;
 fi;
-docker scan --accept-license
-docker scan --token "${SNYK_TOKEN}"
+docker scan --accept-license --token "${SNYK_TOKEN}" --login
 docker scan --version
 export SNYK_ACCEPTED=1;
