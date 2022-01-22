2f66b7119ae894be188bb8ea91d2047d855b8fcd
Thomas Faurbye Nielsen <Archanium@gmail.com>
2022-01-22 13:16:00 +0100

    Updating the build script again a bit

--- php/build.sh
+++ php/build.sh
@@ -7,7 +7,7 @@ if  [[ "${LATEST_TAG}x" = "latestx" ]]; then
 fi
 docker build --quiet --tag "byflou/${PROJECT}:php8.0${POSTFIX}" --build-arg PHP_VERSION=8.0-fpm .
 docker build --quiet --tag "byflou/${PROJECT}:php8.1${POSTFIX}" --build-arg PHP_VERSION=8.1-fpm --tag "byflou/${PROJECT}:${LATEST_TAG}" .
-if [[ "${SNYK_ACCEPTED}x" = "1x" ]]; then
+if [[ "${SNYK_TOKEN}x" != "x" ]]; then
   echo "Scanning: \"byflou/${PROJECT}:php8.0${POSTFIX}\""
   docker scan --file ./Dockerfile "byflou/${PROJECT}:php8.0${POSTFIX}"
   echo "Scanning: \"byflou/${PROJECT}:php8.1${POSTFIX}\""
