dcf06cb32a587abe9140a5064b4a3c0179fa0d44
Thomas Faurbye Nielsen <faurbye@hey.com>
2023-01-18 15:00:24 +0100

    Updating the steps to be run

--- .ops/snyk_accept.sh
+++ .ops/snyk_accept.sh
@@ -7,6 +7,6 @@ fi;
 mkdir -p ~/.docker/cli-plugins && \
 curl https://github.com/docker/scan-cli-plugin/releases/latest/download/docker-scan_linux_amd64 -L -s -S -o ~/.docker/cli-plugins/docker-scan && \
 chmod +x ~/.docker/cli-plugins/docker-scan
-docker scan --accept-license --token "${SNYK_TOKEN}" --login
-docker scan --version
+#docker scan --accept-license --token "${SNYK_TOKEN}" --login
+#docker scan --version
 export SNYK_ACCEPTED=1;
