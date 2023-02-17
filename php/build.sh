717b8589bbe22fedbbb9fd00f618de0aba8558b3
Thomas Faurbye Nielsen <faurbye@hey.com>
2023-02-17 10:47:17 +0100

    fix: fixing the docker build command for the php project

--- php/build.sh
+++ php/build.sh
@@ -5,14 +5,14 @@ POSTFIX="-${LATEST_TAG}"
 if  [[ "${LATEST_TAG}x" = "latestx" ]]; then
   POSTFIX=""
 fi
-docker build --quiet --tag "byflou/${PROJECT}:php8.0${POSTFIX}" --build-arg PHP_VERSION=8.0-fpm .
-docker build --quiet --tag "byflou/${PROJECT}:php8.1${POSTFIX}" --build-arg PHP_VERSION=8.1-fpm --tag "byflou/${PROJECT}:${LATEST_TAG}" .
-if [[ "${SNYK_TOKEN}x" != "x" ]]; then
-  echo "Scanning: \"byflou/${PROJECT}:php8.0${POSTFIX}\""
-  #docker scan --file ./Dockerfile --exclude-base "byflou/${PROJECT}:php8.0${POSTFIX}"
-  echo "Scanning: \"byflou/${PROJECT}:php8.1${POSTFIX}\""
-  #docker scan --file ./Dockerfile --exclude-base "byflou/${PROJECT}:php8.1${POSTFIX}"
-else
-  echo "Not scanning any images"
-fi
-docker push --quiet --all-tags "byflou/${PROJECT}"
+docker build  --tag "byflou/${PROJECT}:php8.0${POSTFIX}" --build-arg PHP_VERSION=8.0-fpm .
+docker build  --tag "byflou/${PROJECT}:php8.1${POSTFIX}" --build-arg PHP_VERSION=8.1-fpm --tag "byflou/${PROJECT}:${LATEST_TAG}" .
+# if [[ "${SNYK_TOKEN}x" != "x" ]]; then
+#   echo "Scanning: \"byflou/${PROJECT}:php8.0${POSTFIX}\""
+#   #docker scan --file ./Dockerfile --exclude-base "byflou/${PROJECT}:php8.0${POSTFIX}"
+#   echo "Scanning: \"byflou/${PROJECT}:php8.1${POSTFIX}\""
+#   #docker scan --file ./Dockerfile --exclude-base "byflou/${PROJECT}:php8.1${POSTFIX}"
+# else
+#   echo "Not scanning any images"
+# fi
+docker push --all-tags "byflou/${PROJECT}"
