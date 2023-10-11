50e0a3ed94a07b52688a538310084879445d9485
Thomas Faurbye Nielsen <faurbye@hey.com>
2023-10-11 07:20:09 +0200

    feat: updating some of the builds

--- php/build.sh
+++ php/build.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env bash
+set -e
 LATEST_TAG=$1;
 PROJECT="php"
 POSTFIX="-${LATEST_TAG}"
-if  [[ "${LATEST_TAG}x" = "latestx" ]]; then
+if  [[ "${LATEST_TAG}x" = "x" || "${LATEST_TAG}x" = "latestx"  ]]; then
   POSTFIX=""
 fi
-docker build --tag "byflou/${PROJECT}:php8.0${POSTFIX}" --build-arg PHP_VERSION=8.0-fpm .
-docker build --tag "byflou/${PROJECT}:php8.1${POSTFIX}" --build-arg PHP_VERSION=8.1-fpm --tag "byflou/${PROJECT}:${LATEST_TAG}" .
+echo "PostFix: $POSTFIX";
+docker build --tag "byflou/${PROJECT}:php8.0${POSTFIX}" --tag "ghcr.io/archanium/${PROJECT}:php8.0${POSTFIX}" --build-arg PHP_VERSION=8.0-fpm .
+#docker build --tag "byflou/${PROJECT}:php8.1${POSTFIX}" --build-arg PHP_VERSION=8.1-fpm --tag "byflou/${PROJECT}:${LATEST_TAG}" .
 # if [[ "${SNYK_TOKEN}x" != "x" ]]; then
 #   echo "Scanning: \"byflou/${PROJECT}:php8.0${POSTFIX}\""
 #   #docker scan --file ./Dockerfile --exclude-base "byflou/${PROJECT}:php8.0${POSTFIX}"
@@ -15,4 +17,5 @@ docker build --tag "byflou/${PROJECT}:php8.1${POSTFIX}" --build-arg PHP_VERSION=
 # else
 #   echo "Not scanning any images"
 # fi
+docker push --all-tags "ghcr.io/archanium/${PROJECT}"
 docker push --all-tags "byflou/${PROJECT}"
