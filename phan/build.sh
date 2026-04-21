85b9a8e308facdd636df8828b0f11b08960e7c83
Thomas Faurbye Nielsen <faurbye@hey.com>
2026-04-21 22:11:00 +0200

    fix: updating phan

--- phan/build.sh
+++ phan/build.sh
@@ -7,9 +7,8 @@ if  [[ "${LATEST_TAG}x" = "x" || "${LATEST_TAG}x" = "latestx"  ]]; then
   POSTFIX=""
 fi
 echo "PostFix: $POSTFIX";
-docker build --tag "archanium/${PROJECT}:8.0${POSTFIX}" --tag "ghcr.io/archanium/${PROJECT}:8.0${POSTFIX}" -f php8.0.Dockerfile .
-docker build --tag "archanium/${PROJECT}:8.1${POSTFIX}" --tag "ghcr.io/archanium/${PROJECT}:8.1${POSTFIX}" -f php8.1.Dockerfile .
-docker build --tag "archanium/${PROJECT}:8.3${POSTFIX}" --tag "ghcr.io/archanium/${PROJECT}:8.3${POSTFIX}" -f php8.3.Dockerfile .
-
-#docker push --all-tags "ghcr.io/archanium/${PROJECT}"
-docker push --all-tags "archanium/${PROJECT}"
+docker buildx create --use
+docker buildx inspect --bootstrap
+docker buildx build --platform linux/amd64,linux/arm64 \
+              --tag "archanium/${PROJECT}:8.3${POSTFIX}" \
+              -f php8.3.Dockerfile --push .
