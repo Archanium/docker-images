a01200d62fa8f43d88442a4584703c47a68f4450
Thomas Faurbye Nielsen <faurbye@hey.com>
2026-04-26 17:51:09 +0200

    fix: updating the docker build process

--- circleci-ext/build.sh
+++ circleci-ext/build.sh
@@ -1,4 +1,14 @@
 #!/usr/bin/env bash
-docker build --tag "archanium/dreamshop-php:8.0" --push --file Dockerfile .
-docker build --tag "archanium/dreamshop-php:8.0-browsers" --push --file browsers.Dockerfile .
-docker build --tag "archanium/dreamshop-builder:8.0"  --push --file builder.Dockerfile .
+docker buildx create --use
+docker buildx build \
+   --cache-from type=registry,ref=archanium/dreamshop-php:8.0-buildcache \
+   --cache-to type=registry,ref=archanium/dreamshop-php:8.0-buildcache,mode=max \
+   --tag "archanium/dreamshop-php:8.0" --push --file Dockerfile  .
+docker buildx build \
+   --cache-from type=registry,ref=archanium/dreamshop-php:8.0-browsers-buildcache \
+   --cache-to type=registry,ref=archanium/dreamshop-php:8.0-browsers-buildcache,mode=max \
+   --tag "archanium/dreamshop-php:8.0-browsers" --push --file browsers.Dockerfile  .
+docker buildx build \
+   --cache-from type=registry,ref=archanium/dreamshop-builder:8.0 \
+   --cache-to type=registry,ref=archanium/dreamshop-builder:8.0,mode=max \
+   --tag "archanium/dreamshop-builder:8.0" --push --file builder.Dockerfile  .
